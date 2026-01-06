2e22eddb37b849de4bdd62858d53e0b9e3c76db8
cris959 <christiangaray959@gmail.com>
2026-01-05 21:11:37 -0300

    Agregando dependencia security-creando entidad Usuario-creando migration 6-tabla usuarios

---
+++ src/main/java/com/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,25 @@
+package com.med.voll.api.domain.usuario;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String contrasena;
+}
