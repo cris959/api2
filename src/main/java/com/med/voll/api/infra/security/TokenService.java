bdf563623f1bf7d3d6fb7dbe6ccaceb7e72ad6ea
cris959 <christiangaray959@gmail.com>
2026-01-08 11:46:12 -0300

    Agregando configuracion del token

--- src/main/java/com/med/voll/api/infra/security/TokenService.java
+++ src/main/java/com/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,19 @@ public class TokenService {
 
     private Instant fechaExpiracion() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido o expirado!");
+        }
     }
 }
